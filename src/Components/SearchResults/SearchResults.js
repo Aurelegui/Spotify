6e1a0a3090f0cb34f450bf3010fe1f6b1dc6dfc3
Aurele <83721648+Aurelegui@users.noreply.github.com>
2021-06-18 19:22:21 +0200

    Add files via upload

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,19 +2,17 @@ import React from 'react';
 import './SearchResults.css';
 import TrackList from '../TrackList/TrackList';
 
-class SearchResults extends React.Component {
-    render() {
-        return (
-            <div className="SearchResults">
-                <h2>Results</h2>
+export default function SearchResults(props) {
 
-                <TrackList tracks={this.props.searchResults}
-                    onAdd={this.props.onAdd}
-                    isRemoval={false}
-                />
-            </div>
-        );
-    }
+    return (
+        <div className="SearchResults">
+            <h2>Results</h2>
+
+            <TrackList
+                tracks={props.searchResults}
+                onAdd={props.onAdd}
+                isRemoval={false}
+            />
+        </div>
+    );
 }
-
-export default SearchResults;
