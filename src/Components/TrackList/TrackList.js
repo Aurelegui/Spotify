0ea75a9ec607fd1e476281e613f0e40095a6a340
Aurele <83721648+Aurelegui@users.noreply.github.com>
2021-06-18 19:22:49 +0200

    Add files via upload

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,23 +2,20 @@ import React from 'react';
 import './TrackList.css';
 import Track from '../Track/Track';
 
-class TrackList extends React.Component {
-    render() {
-        return (
-            <div className="TrackList">
-                {
-                    this.props.tracks.map(track => {
-                        return <Track track={track}
-                            key={track.id}
-                            onAdd={this.props.onAdd} 
-                            onRemove={this.props.onRemove}
-                            isRemoval={this.props.isRemoval}
-                            />
-                    })
-                }
-            </div>
-        )
-    }
-}
+export default function TrackList(props) {
 
-export default TrackList;
+    return (
+        <div className="TrackList">
+            {
+                    () => props.tracks.map(track => {
+                    return <Track track={track}
+                    key={track.id}
+                    onAdd={props.onAdd} 
+                    onRemove={props.onRemove}
+                    isRemoval={props.isRemoval}
+                    />
+                })
+            }
+        </div>
+    );
+}
